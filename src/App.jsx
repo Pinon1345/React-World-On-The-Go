0f24319a12841c9ac7e7b2dae2ac1769f801e2bb
Pinon1345 <pinonfurkan1@gmail.com>
2026-04-27 17:53:51 +0600

    starting the project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Countries from './components/countries/Countries'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-center first-heading'>REACT WORLD</h1>
+      <Countries></Countries>
+
+    </>
+  )
+}
+
+export default App
